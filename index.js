3fd87a7f4366cd1eadbfe801b5c3667926b742b5
boyraf <raphael.kymotho@gmail.com>
2023-03-21 13:22:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let main = document.querySelector('main');
+main.remove();
+const newHeader = document.createElement("h1");
+//document.body.append(newHeader);
+//newHeader.nodeName = 'h1';
+newHeader.id = 'victory';
+newHeader.innerHTML = 'YOUR-NAME is the champion'
